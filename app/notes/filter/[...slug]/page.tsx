e7a79bffd8dd171c4265b6681e111aa4a3f738f8
Liudmyla Pominchuk <stomaludmila537@gmail.com>
2025-09-04 09:12:30 +0300

    Initial commit

---
+++ app/notes/filter/[...slug]/page.tsx
@@ -0,0 +1,29 @@
+import { dehydrate, HydrationBoundary, QueryClient } from "@tanstack/react-query";
+import NotesClient from "./Notes.client";
+import { fetchNotes } from "@/lib/api";
+import { NoteTag } from "@/types/note";
+
+type Props = {
+  params: Promise<{ slug: string[] }>;
+}
+
+export default async function NotesPage ({ params }: Props) {
+  const { slug } = await params;
+  const queryClient = new QueryClient();
+
+  const tag = slug[0] === "All" ? undefined : (slug[0] as NoteTag);
+  
+  await queryClient.prefetchQuery({
+    queryKey: ["notes", "", 1, tag],
+    queryFn: () => fetchNotes({page: 1, perPage: 12, search: "", tag}),
+  })
+
+  return (
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <NotesClient tag={tag} />
+    </HydrationBoundary>
+  )
+};
+
+
+
---
+++ components/NoteForm/NoteForm.tsx
@@ -0,0 +1,109 @@
+import css from "./NoteForm.module.css";
+import { Formik, Form, Field, FormikHelpers, ErrorMessage } from "formik";
+import * as Yup from "yup";
+import { CreateNote, NoteTag } from "@/types/note";
+import { createNote } from "@/lib/api";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+
+interface NoteFormValues {
+  title: string;
+  content: string;
+  tag: NoteTag;
+}
+
+const initialValues: NoteFormValues = {
+  title: "",
+  content: "",
+  tag: "Todo",
+};
+
+const tags: NoteTag[] = ["Todo", "Work", "Personal", "Meeting", "Shopping" ];
+
+const validationSchema = Yup.object().shape({
+  title: Yup.string()
+    .min(3, "Title min 3 chars")
+    .max(50, "Title max 50 chars")
+    .required("Title is required"),
+  content: Yup.string().max(500, "Content max 500 chars"),
+  tag: Yup.string().oneOf(tags, "Invalid tag").required("Required"),
+});
+
+interface NoteFormProps {
+  onCancel: () => void;
+}
+
+export default function NoteForm({ onCancel }: NoteFormProps) {
+  const queryClient = useQueryClient();
+
+  const createMutation = useMutation({
+    mutationFn: (data: CreateNote) => createNote(data),
+    onSuccess: () => {
+      toast("Note created succesfuly!!!")
+      queryClient.invalidateQueries({ queryKey: ["notes"] });
+      onCancel();
+    },
+    onError: (error) => {
+      console.error("Create error:", error);
+    },
+  });
+
+  const handleSubmit = (
+    values: NoteFormValues,
+    actions: FormikHelpers<NoteFormValues>
+  ) => {
+    console.log("Order data:", values);
+    createMutation.mutate(values);
+    actions.resetForm();
+  };
+
+  return (
+    <Formik
+      initialValues={initialValues}
+      onSubmit={handleSubmit}
+      validationSchema={validationSchema}
+    >
+      <Form className={css.form}>
+        <div className={css.formGroup}>
+          <label htmlFor="title">Title</label>
+          <Field id="title" type="text" name="title" className={css.input} />
+
+          <ErrorMessage name="title" className={css.error} component="span" />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor="content">Content</label>
+          <Field
+            as="textarea"
+            id="content"
+            name="content"
+            rows={8}
+            className={css.textarea}
+          />
+          <ErrorMessage name="content" component="span" className={css.error} />
+        </div>
+
+        <div className={css.formGroup}>
+          <label htmlFor="tag">Tag</label>
+          <Field as="select" id="tag" name="tag" className={css.select}>
+            <option value="Todo">Todo</option>
+            <option value="Work">Work</option>
+            <option value="Personal">Personal</option>
+            <option value="Meeting">Meeting</option>
+            <option value="Shopping">Shopping</option>
+          </Field>
+          <ErrorMessage name="tag" component="span" className={css.error} />
+        </div>
+
+        <div className={css.actions}>
+          <button type="button" className={css.cancelButton} onClick={onCancel}>
+            Cancel
+          </button>
+          <button type="submit" className={css.submitButton} disabled={false}>
+            {createMutation.isPending ? "Creating..." : "Create note"}
+          </button>
+        </div>
+      </Form>
+    </Formik>
+  );
+}
